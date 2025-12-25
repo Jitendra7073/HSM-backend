5fac504d2d5b64a5526903900d8f29ec029c4fcd
jssuthar-alt <jssuthar@enacton.email>
2025-12-25 16:09:12 +0530

    firebase admin sdk configuration keys added into env

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,9 +1,22 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./firebase-admin-sdk.json");
+const serviceAccount = {
+  type: process.env.FIREBASE_ADMIN_SDK_TYPE,
+  project_id: process.env.FIREBASE_ADMIN_SDK_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_ADMIN_SDK_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_ADMIN_SDK_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_ADMIN_SDK_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_ADMIN_SDK_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_ADMIN_SDK_AUTH_URI,
+  token_uri: process.env.FIREBASE_ADMIN_SDK_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_ADMIN_SDK_AUTH_PROVIDER_X_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_ADMIN_SDK_CLIENT_X_CERT_URL,
+  universe_domain: process.env.FIREBASE_ADMIN_SDK_UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
